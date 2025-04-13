eafd5b2fc4c703443262b6acb29910b2bf9a36c1
polyanskiy <polyanskiy.main@list.ru>
2025-04-14 01:48:10 +0300

    Запуск проекта

--- TestTaskDaData.AddressProcessApi/Program.cs
+++ TestTaskDaData.AddressProcessApi/Program.cs
@@ -1,41 +1,59 @@
+using Serilog;
+using System.Reflection;
+using TestTaskDaData.AddressProcessApi.Api.Middlewares;
+using TestTaskDaData.AddressProcessApi.Connectors.DaData;
+using TestTaskDaData.AddressProcessApi.Services.AddressProcessService;
+using TestTaskDaData.AddressProcessApi.Services.PopulationService;
+using TestTaskDaData.AddressProcessApi.Shared.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Host.UseSerilog((ctx, lc) => lc
+    .ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.Configure<ServiceOptions>(
+    builder.Configuration.GetSection("DaDataOptions"));
+
+builder.Services.AddHttpClient<IDaDataClient, DaDataClient>((sp, client) =>
+{
+    var options = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<ServiceOptions>>().Value;
+    client.BaseAddress = new Uri(options.BaseUrl!);
+    client.DefaultRequestHeaders.Add("Authorization", $"Token {options.Token}");
+    client.DefaultRequestHeaders.Add("X-Secret", options.Secret);
+});
+
+builder.Services.AddScoped<IPopulationService, StubPopulationService>();
+builder.Services.AddScoped<IAddressProcessService, DaDataAddressProcessService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("DefaultPolicy");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();    
