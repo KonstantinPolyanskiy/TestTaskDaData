c272903addd91c3b35654bbfa5e5dc5fc123ef15
polyanskiy <polyanskiy.main@list.ru>
2025-04-14 04:56:58 +0300

    Разнес классы ответов/запросов по разным файлам

--- TestTaskDaData.AddressProcessApi/Shared/Models/DaDataResponse.cs
+++ TestTaskDaData.AddressProcessApi/Shared/Models/DaDataResponse.cs
@@ -58,24 +58,3 @@ public class DaDataResponse
     [JsonPropertyName("house_flat_count")]
     public int? HouseFlatCount { get; set; }
 }
-
-/// <summary>
-/// Ответ нашего сервиса 
-/// </summary>
-public class ProcessedAddress
-{
-    #region Стандартные поля из ТЗ
-
-    public string? Country { get; set; }
-    public string? Region { get; set; }
-    public string? City { get; set; }
-    public string? Street { get; set; }
-    public string? House { get; set; }
-
-    #endregion
-    
-    /// <summary>
-    /// Доп. поле - приблизительное кол-во жильцов в доме
-    /// </summary>
-    public double? ApproxHouseResidents { get; set; }
-}
